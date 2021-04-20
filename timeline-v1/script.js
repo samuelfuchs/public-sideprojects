b9bde02a33eaa2318696432416c1c2bd7016e418
Samuel Fuchs <fuchsgerson@gmail.com>
2021-04-20 17:34:15 -0300

    Lang changed
    I changed the language and added some interesting info about myself.

--- timeline-v1/script.js
+++ timeline-v1/script.js
@@ -1,15 +1,15 @@
-function myFunction() {
+function btnShowMore() {
 	const dots = document.getElementById('dots')
 	const moreText = document.getElementById('more')
 	const btnText = document.getElementById('myBtn')
 
 	if (dots.style.display === 'none') {
 		dots.style.display = 'inline'
-		btnText.innerHTML = 'Read more'
+		btnText.innerHTML = 'Leia mais'
 		moreText.style.display = 'none'
 	} else {
 		dots.style.display = 'none'
-		btnText.innerHTML = 'Read less'
+		btnText.innerHTML = 'Ocultar'
 		moreText.style.display = 'inline'
 	}
 }
