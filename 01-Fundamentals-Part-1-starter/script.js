0abbab08d8ac4fd8e90602d110b84787ce865427
Samuel Fuchs <fuchsgerson@gmail.com>
2021-04-29 14:33:02 -0300

    Equality Operators: == vs. ===

--- 01-Fundamentals-Part-1-starter/script.js
+++ 01-Fundamentals-Part-1-starter/script.js
@@ -162,7 +162,7 @@ console.log('23' > '18')
 let n = '1' + 1
 n = n - 1
 console.log(n)
-*/
+
 // Truthy and Falsy Values
 const money = 0
 if (money) {
@@ -177,3 +177,23 @@ if (height) {
 } else {
 	console.log('Height is UNDEFINED')
 }
+*/
+// Equality Operators: == vs. ===
+const age = '18'
+if (age === 18) console.log('You just became an adult :D (strict)')
+if (age == 18) console.log('You just became an adult :D (loose)')
+
+const favourite = Number(prompt("What's your favourite number?"))
+console.log(favourite)
+
+if (favourite === 23) {
+	console.log('Cool! 23 is an amazing number!')
+} else if (favourite === 7) {
+	console.log('7 is also a cool number')
+} else if (favourite === 9) {
+	console.log('9 is also a super cool number')
+} else {
+	console.log('Number is not 23 or 7 or 9')
+}
+
+if (favourite !== 23) console.log('Why not 23?')
