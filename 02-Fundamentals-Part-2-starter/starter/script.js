afc82b78849875b5032f87e6278fde4542636ade
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-01 14:08:25 -0300

    use strict

---
+++ 02-Fundamentals-Part-2-starter/starter/script.js
@@ -0,0 +1,23 @@
+'use strict'
+/*
+let hasDriversLicense = false
+const passTest = true
+
+if (passTest) hasDriverLicense = true
+if (hasDriversLicense) console.log('I can drive!!')
+
+
+const interface = 'Audio'
+const private = 534
+
+// Is a special mode to write more secure JS code
+// Has to be very first statement in the script
+// Possibility to apply to a single function
+// Avoids accidental errors
+// 1st: forbids us to do certain things
+// 2nd: creates visible errors in the console where normal JS mode would
+// have failed silently
+*/
+// ====
+// Functions
+// ====
