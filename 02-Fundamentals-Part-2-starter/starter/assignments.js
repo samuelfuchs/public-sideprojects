cc7279c98a96b4296f52c8e0b3d911257f4e67b7
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-04 01:40:32 -0300

    Function Declarations vs. Expressions assignment

--- 02-Fundamentals-Part-2-starter/starter/assignments.js
+++ 02-Fundamentals-Part-2-starter/starter/assignments.js
@@ -18,3 +18,22 @@ describeCountry('Canada', 37, 'Ottawa')
 // ====
 // Function Declarations vs. Expressions
 // ====
+// Func declaration:
+function percentageOfWorld1(population) {
+	return (population / 7900) * 100
+}
+
+const brazil1 = percentageOfWorld1(211)
+const argentina1 = percentageOfWorld1(45)
+const canada1 = percentageOfWorld1(37)
+console.log(brazil1, argentina1, canada1)
+
+// Func expression:
+const percentageOfWorld2 = function (population) {
+	return (population / 7900) * 100
+}
+
+const brazil2 = percentageOfWorld2(211)
+const argentina2 = percentageOfWorld2(45)
+const canada2 = percentageOfWorld2(37)
+console.log(brazil2, argentina2, canada2)
