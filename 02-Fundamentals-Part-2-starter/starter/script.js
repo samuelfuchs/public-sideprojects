0c92cdd93aeee3b37b63ed9d3ab4e7d4a9f76812
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-07 14:45:44 -0300

    minor changes

--- 02-Fundamentals-Part-2-starter/starter/script.js
+++ 02-Fundamentals-Part-2-starter/starter/script.js
@@ -510,7 +510,8 @@ for (let i = 0; i < jonas.length; i++) {
 	if (typeof jonas[i] === 'number') break
 	console.log(jonas[i], typeof jonas[i])
 }
-*/
+
 // ====
 // Looping Backwards and Loops in Loops
 // ====
+*/
