0937b3950271c36e13fc55e5f3646cd38618ec19
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-12 20:47:47 -0300

    Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,14 +11,27 @@
 /* ===========
 Selecting and Manipulating Elements
 =========== */
-document.querySelector('.message').textContent = 'Correct Number! 🎉';
+// document.querySelector('.message').textContent = 'Correct Number! 🎉';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-// .value to get a value. Right now it is empty
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// // .value to get a value. Right now it is empty
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
 /* ===========
 Handling Click Events
 =========== */
+// Event is something that happens on the page. Click, mouse moving, etc
+
+// Select the element where the event should happen
+// 'click' is the type of the event, the second arguement is a func
+// Remember: a func is just an value, so we put it in here
+// The func will only be called when the event happens
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
