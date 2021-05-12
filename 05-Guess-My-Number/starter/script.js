ec793c3a7d9d965c098501bea3b751ab79e6f677
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-12 20:35:51 -0300

    Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
 // DOM
 // Document Object Model: Structured representation of HTML Documents. Allows JS to access HTML elements and styles to manipulate them.
@@ -8,3 +8,17 @@ console.log(document.querySelector('.message').textContent);
 // DOM !== JS
 // it's part of web api - labraries also written in JS
 // API - Application Programming Interface
+/* ===========
+Selecting and Manipulating Elements
+=========== */
+document.querySelector('.message').textContent = 'Correct Number! 🎉';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+// .value to get a value. Right now it is empty
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+/* ===========
+Handling Click Events
+=========== */
