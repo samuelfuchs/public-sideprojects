b1058f2e0261d46c38c083d2d0bf629030481aa2
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-12 23:33:38 -0300

    Implementing Highscores

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -43,17 +43,7 @@ Implementing the Game Logic / Manipulating CSS Styles
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 // this will change, part of the state of the code
 let score = 20;
-
-document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-});
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -68,6 +58,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -90,6 +85,18 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
 /* ===========
 Manipulating CSS Styles
 =========== */
@@ -97,3 +104,7 @@ Manipulating CSS Styles
 // .style.background-color -> is not allowed. We write it in camel-case JS-syntax:
 // backgroundColor
 // when specifying style, it is always a string.
+
+/* ===========
+Implementing Highscores
+=========== */
