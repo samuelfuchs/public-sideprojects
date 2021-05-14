874c7f2376f8d5f9244ed19dda3761ce3082bfd3
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-14 08:11:57 -0300

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -114,6 +114,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
+  // change back the elements
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
