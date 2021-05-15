02278fdb2b1a39f8e64676165a33bce97dafd60d
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-15 08:38:39 -0300

    modal project

---
+++ 06-modal/starter/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelector('.open-modal');
+
+console.log(btnsOpenModal);
