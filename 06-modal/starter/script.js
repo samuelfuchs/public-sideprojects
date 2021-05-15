0b5e07891375c92c5285c22663179b7daf7336d3
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-15 08:41:33 -0300

    PROJECT #2: Modal Window

--- 06-modal/starter/script.js
+++ 06-modal/starter/script.js
@@ -3,6 +3,10 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.open-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+}
