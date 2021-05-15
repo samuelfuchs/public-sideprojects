3963372ef7fa709c9f1f1a57b862a84826201070
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-15 09:20:06 -0300

    Working With Classes

--- 06-modal/starter/script.js
+++ 06-modal/starter/script.js
@@ -5,8 +5,22 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  console.log('Button clicked');
+  // we don't put a '.' before hidden. This is only to select things. We already made our selection
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
