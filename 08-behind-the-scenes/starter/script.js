fcae55aa46e08cf14e16b411d629c8d7dc41ab9b
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-18 22:34:03 -0300

    Scoping in Practice

--- 08-behind-the-scenes/starter/script.js
+++ 08-behind-the-scenes/starter/script.js
@@ -1,2 +1,28 @@
 'use strict';
-// Lecture90
+
+/* ==========
+Scoping in Practice
+========== */
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
