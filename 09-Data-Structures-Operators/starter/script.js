4629bb98e0a5d02e55f17fb9d0bd60dd96764fcd
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-22 08:41:51 -0300

    Destructuring Arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,46 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// destructuring assignment:
+// don't forget to declare the variables with const
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+// to 'jump' one element and select the next, simply put an empty space:
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching variables
+// to switch variable before we had to create a temp variable:
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+// Usin destructuring:
+// we do not need to use const because we are just reassigning the variables
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//Receive 2 return values from function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested destructuring
+// nested array-an array inside in an array
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+// giving default values so we don't get undefined
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
