aafb4d6a25b6dc97d2fb368f540f89c95026487f
Samuel Fuchs <fuchsgerson@gmail.com>
2021-05-27 03:52:25 -0300

    Coding Challenge #3 - solution

--- 09-Data-Structures-Operators/starter/codingchallenge.js
+++ 09-Data-Structures-Operators/starter/codingchallenge.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*const game = {
   team1: 'Bayern Munich',
   team2: 'Borussia Dortmund',
@@ -39,6 +41,50 @@
   },
 };
 */
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+// =====================
+// Coding Challenge #3
+// =====================
+
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+// const events = new Set(gameEvents.values()); // 1st: Get only the values
+const events = [...new Set(gameEvents.values())]; // 2nd: create the spread operator to unpack the set
+console.log(events);
+
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+gameEvents.delete(64);
+
+// 3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+// Pop takes only the last element
+const time = [...gameEvents.keys()].pop(); // The [...xxx] is to create an array
+
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽ GOAL
+
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[ ${half} HALF ] ${min}: ${event}`);
+}
+
 // =====================
 // Coding Challenge #2
 // =====================
