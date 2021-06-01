1aa978240ee94d23732f9b6b3ff24a34615910e2
Samuel Fuchs <fuchsgerson@gmail.com>
2021-06-01 03:27:32 -0300

    Coding Challenge #1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -5,81 +5,81 @@
 // Also allows us to manually set the 'this' keyword
 // Bind does not imediatly call the function, instead it returns a new function where the 'this' keyword is bound
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  // old syntax: book: function(){}
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
-};
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   // old syntax: book: function(){}
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
 
-const book = lufthansa.book;
+// const book = lufthansa.book;
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
 
-const swiss = {
-  airline: 'Swiss Air Lines',
-  iataCode: 'LX',
-  bookings: [],
-};
+// const swiss = {
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
 
-const bookEW = book.bind(eurowings); // returns a NEW func where 'this' is set to eurowings
-const bookLH = book.bind(lufthansa);
-const bookLX = book.bind(swiss);
+// const bookEW = book.bind(eurowings); // returns a NEW func where 'this' is set to eurowings
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
 
-bookEW(222, 'Steven Williams'); // we no longer need to specify the 'this' keyword
+// bookEW(222, 'Steven Williams'); // we no longer need to specify the 'this' keyword
 
-const bookEW23 = book.bind(eurowings, 23); // setting arguments 'in stone'
-bookEW23('Jonas Schmedtmann');
-bookEW23('Martha Cooper');
+// const bookEW23 = book.bind(eurowings, 23); // setting arguments 'in stone'
+// bookEW23('Jonas Schmedtmann');
+// bookEW23('Martha Cooper');
 
-// PARTIAL APLICATION: a part of the argument of the original functions is already aplied (already set)
+// // PARTIAL APLICATION: a part of the argument of the original functions is already aplied (already set)
 
-// Another useful way to use bind method is with eventlisteners:
-// With Event Listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  console.log(this);
+// // Another useful way to use bind method is with eventlisteners:
+// // With Event Listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
 
-  this.planes++;
-  console.log(this.planes);
-};
+//   this.planes++;
+//   console.log(this.planes);
+// };
 
-// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane); // the buttun itself is the 'this' keyword, which is NaN. It always depends WHO is calling the function. In this case, it is the Event Listener. We need to manually define the 'this' keyword
+// // document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane); // the buttun itself is the 'this' keyword, which is NaN. It always depends WHO is calling the function. In this case, it is the Event Listener. We need to manually define the 'this' keyword
 
-// the correct way is to use the bind method:
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // 'this' now points to lufthansa
+// // the correct way is to use the bind method:
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // 'this' now points to lufthansa
 
-// Another example about partial aplication:
-// many times we are not even interested in the 'this' keyword, but we still use bind() to do this.
-// Partial aplication means we can preset our aplication
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
+// // Another example about partial aplication:
+// // many times we are not even interested in the 'this' keyword, but we still use bind() to do this.
+// // Partial aplication means we can preset our aplication
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
 
-const addVAT = addTax.bind(null, 0.23); // sets rate value in stone. As we dont care about 'this', we put in null
+// const addVAT = addTax.bind(null, 0.23); // sets rate value in stone. As we dont care about 'this', we put in null
 
-console.log(addVAT(100));
-console.log(addVAT(23));
+// console.log(addVAT(100));
+// console.log(addVAT(23));
 
-const addTaxRate = function (rate) {
-  return function (value) {
-    return value + value * rate;
-  };
-};
-const addVAT2 = addTaxRate(0.23);
-console.log(addVAT2(100));
-console.log(addVAT2(23));
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+// const addVAT2 = addTaxRate(0.23);
+// console.log(addVAT2(100));
+// console.log(addVAT2(23));
 
 // ===============
 // The call and apply Methods
