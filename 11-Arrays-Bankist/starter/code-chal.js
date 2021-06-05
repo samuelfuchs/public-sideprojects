9402e10f9168f3a78b4e8c7585d4ba470681cbb2
Samuel Fuchs <fuchsgerson@gmail.com>
2021-06-05 07:07:43 -0300

    Coding Challenge #2

--- 11-Arrays-Bankist/starter/code-chal.js
+++ 11-Arrays-Bankist/starter/code-chal.js
@@ -22,3 +22,40 @@
 // };
 // // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 // checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// ==========
+// Coding Challenge #2
+// ==========
+
+// 1.
+// Data 1: [5, 2, 4, 1, 15, 8, 3]
+// Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+
+const avgHumanAgeMap = data1.map(function (mov, i) {
+  if (mov <= 2) {
+    return mov * 2;
+  } else {
+    return 16 + mov * 4;
+  }
+});
+console.log(avgHumanAgeMap);
+// 2.
+const adultDogs = avgHumanAgeMap.filter(function (mov) {
+  return mov >= 18;
+});
+console.log(adultDogs);
+
+// 3.
+const avgAgeAdult = adultDogs.reduce(function (acc, mov) {
+  return (acc + mov) / adultDogs.length;
+}, adultDogs[0]);
+console.log(avgAgeAdult);
+
+/*
+console.log(movements);
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  return acc + cur;
+});
+*/
