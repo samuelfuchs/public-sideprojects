e2d829fa2abaa7376c8a809e363d38f8198c3bf4
Samuel Fuchs <fuchsgerson@gmail.com>
2021-06-08 05:54:07 -0300

    Update code-chal.js

--- 11-Arrays-Bankist/starter/code-chal.js
+++ 11-Arrays-Bankist/starter/code-chal.js
@@ -78,29 +78,29 @@
 // Coding Challenge #3
 // ==========
 
-// const calcAverageHumanAge2 = function (ages) {
-//   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+const calcAverageHumanAge2 = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
 
-//   const adults = humanAges.filter(age => age >= 18);
-//   console.log(humanAges);
-//   console.log(adults);
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
 
-//   //   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
-//   // different way to calculate avg:
-//   const average = adults.reduce(
-//     (acc, age, i, arr) => acc + age / arr.length,
-//     0
-//   );
-//   return average;
-// };
+  //   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  // different way to calculate avg:
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
+  return average;
+};
 
-// const calcAverageHumanAge = ages =>
-//   ages
-//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
-//     .filter(age => age >= 18)
-//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
-// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 
-// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
-// console.log(avg1, avg2);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
