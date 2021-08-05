30e0b40abcc5a68c76695c2b0a232a8972dd35b2
Samuel Fuchs <fuchsgerson@gmail.com>
2021-08-04 23:52:46 -0300

    Implementing Smooth Scrolling

--- new-omnifood-responsive-js/js/script.js
+++ new-omnifood-responsive-js/js/script.js
@@ -28,3 +28,29 @@ const headerEl = document.querySelector('.header')
 btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open')
 })
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link')
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+    const href = link.getAttribute('href')
+
+    // Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href)
+      sectionEl.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    // close mobile navigation
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open')
+  })
+})
